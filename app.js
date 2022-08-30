d560a1cd23323c070fbc6a1e702777233ac4ce8d
Fernando Luna <fer22luna@gmail.com>
2022-08-29 21:37:29 -0300

    Segunda entrega comentarios

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+
+
+// Defino el Dom  capturo los eventos
+
 const btnComprarLeche = document.getElementById("btn-1")
 const btnComprarQueso = document.getElementById("btn-2")
 const btnComprarManteca = document.getElementById("btn-3")
@@ -9,13 +13,15 @@ const mostrarLista = document.getElementById("MostrarLista")
 const total = document.getElementById("total")
 const mostrarUnProducto = document.getElementById("mostrarUnProducto")
 
+// Lista de precios que proximamente los pondre en el JSON 
+
 const precioLeche = 150;
 const precioQueso = 600;
 const precioManteca = 200;
 const precioHuevo = 300;
 const precioSoda = 150;
 
-
+// Armo dos arrays uno con los productos y otro con los precios
 
 const listaProd =[];
 const precio = [];
@@ -47,7 +53,8 @@ btnComprarSoda.addEventListener("click",() => {
     precio.push(eval("precio"+`${btnComprarSoda.value}`))
 })
 
-
+// Aca hice esta funcion ya que no podia capturar todos los botones tuve que hace una iteracion con un for
+// Para que me muestre el ultimo boton seleccionado
 
 function boton(className){
     var elements = document.getElementsByClassName(className);
@@ -58,8 +65,10 @@ function boton(className){
         })
     }}
 
-boton("btn")
+boton("btn") // llamo a la funcion
 
+
+ // Me fabrico un constructor para pushear los valores e los arrays
 class OtroObjeto  {
     constructor(nombre, precio) {
         this.nombre = nombre;
@@ -81,14 +90,15 @@ objeto.push(new OtroObjeto(listaProd[i],precio[i]))
 
 }
 sumaTotal()
-btnTotal.style.display = "none" 
+btnTotal.style.display = "none"  ///  aca no se me ocurrio otra cosa que sacar el boton, ya que al volverle el click
+                                ///  se vulve a sumar sobre el array 
 }
 )
-
+// funcion para sumar
 function sumaTotal() {
 for  (  let i = 0; i < precio.length; i++) {
     cantidadFinal += precio[i] 
-total.innerHTML = `<h2><span>El total es: </span>${cantidadFinal}</h2>`
+total.innerHTML = `<h2><span> El total es:  </span>${cantidadFinal}</h2>`
 }}
 
 
@@ -97,7 +107,7 @@ btnConfirmar.addEventListener("click", () => {
     if (objeto.length === 0) {
 
         Swal.fire(
-            'Carro Vacio!',
+            'Carro esta Vacio!',
             'Porfavor ! Debe agregar productos',
             'Error'
           )
